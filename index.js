3a3bdf114dedfd408e6b61a81c16e52b90566c14
Jadon <1tenletters0@gmail.com>
2025-01-03 12:46:38 -0800

    responsiveness done! Just some final tweaks

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 let constrastToggle = false;
 
 function toggleContrast() {
@@ -6,5 +8,15 @@ function toggleContrast() {
         document.body.classList += " dark-theme"
     } else {
         document.body.classList.remove("dark-theme")
+    }
+}
+
+changeIcon = (icon) => icon.classList.toggle('fa-sun')
+
+function toggleClass(fa) {
+    if (fa.classList == "fa-solid fa-moon") {
+        fa.classList = "fa-solid fa-sun";
+    } else {
+        fa.classList = "fa-solid fa-moon"
     }
 }
