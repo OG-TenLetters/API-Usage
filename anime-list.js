5b1d6730a2d8b818bd3fefc3f34485b330f57010
Jadon <1tenletters0@gmail.com>
2025-03-03 13:52:06 -0800

    Modal Integration Fail, wrong placement and class changes unresponsive

--- anime-list.js
+++ anime-list.js
@@ -1,3 +1,5 @@
+let isModalOpen = false;
+
 function openMenu() {
     document.body.classList += " menu--open"
 }
@@ -5,6 +7,51 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu--open')
 }
+
+function contact(event) {
+    event.preventDefault();
+  
+    const loading = document.querySelector(".modal__overlay--loading");
+  
+    const success = document.querySelector(".modal__overlay--success");
+  
+    loading.classList += " modal__overlay--visible";
+  
+    emailjs
+  
+      .sendForm(
+        "service_dmf4e7a",
+        "template_acfbkmf",
+        event.target,
+        "qVGgK80-uMkkItvbU"
+      )
+  
+      .then(() => {
+        loading.classList.remove("modal__overlay--visible");
+  
+        success.classList += " modal__overlay--visible";
+      })
+  
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+  
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly at 1tenletters0@gmail.com"
+        );
+      });
+  }
+
+function toggleModal() {
+    if (isModalOpen) {
+      isModalOpen = false;
+      return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+  
+    //toggle modal
+    document.body.classList += " modal--open";
+  }
+  
 
 let constrastToggle = false;
 
--- index.js
+++ index.js
@@ -1,4 +1,49 @@
+let isModalOpen = false;
 
+function contact(event) {
+    event.preventDefault();
+  
+    const loading = document.querySelector(".modal__overlay--loading");
+  
+    const success = document.querySelector(".modal__overlay--success");
+  
+    loading.classList += " modal__overlay--visible";
+  
+    emailjs
+  
+      .sendForm(
+        "service_dmf4e7a",
+        "template_acfbkmf",
+        event.target,
+        "qVGgK80-uMkkItvbU"
+      )
+  
+      .then(() => {
+        loading.classList.remove("modal__overlay--visible");
+  
+        success.classList += " modal__overlay--visible";
+      })
+  
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+  
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly at 1tenletters0@gmail.com"
+        );
+      });
+  }
+  
+
+function toggleModal() {
+    if (isModalOpen) {
+      isModalOpen = false;
+      return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+  
+    //toggle modal
+    document.body.classList += " modal--open";
+  }
 
 let constrastToggle = false;
 
