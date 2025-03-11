b166a68859e7a24b9c138adf77ea725a818fa195
Jadon <1tenletters0@gmail.com>
2025-03-11 13:33:13 -0700

    It's working!! There was quite a bit to figure out but it's getting there! Top 10 works, The First 4 work, and so does the rest. I want to clean it up so the code is smaller though...

--- anime-list.js
+++ anime-list.js
@@ -2,42 +2,120 @@
 // Top 10 API: https://api.jikan.moe/v4/top/anime
 let isModalOpen = false;
 let lastScrollY = 0;
-const animeHTMLEl = document.querySelector(".content__rank-box");
+const animeRankHTMLEl = document.querySelector(".contents__rank-box");
 
-async function main() {
-  const anime = await fetch("https://api.jikan.moe/v4/top/anime");
-  const animeData = await anime.json();
-  console.log(animeData);
-  animeHTMLEl.innerHTML = animeData.map((data) => animeHTML(data)).join("");
+async function animeRankMain() {
+  const animeRank = await fetch(`https://api.jikan.moe/v4/top/anime`);
+  const animeRankPortal = await animeRank.json();
+  const animeRankData = animeRankPortal.data.slice(0, 10);
+  // console.log(animeRankData);
+  animeRankHTMLEl.innerHTML = animeRankData.map((data, index) => animeRankHTML(data, index)).join("");
+  // console.log(animeRankHTMLEl)
+}
+animeRankMain();
+function animeRankHTML(data, index) {
+  return `
+  <div class="content__rank-box" ${data.id}>
+    <div class="content__rank--wrapper">
+      <figure class="content__rank--img--wrapper">
+        <img
+        src="${data.images.webp.image_url}"
+        class="content__rank--img"
+        />
+        <div class="content__rank">${index + 1}</div>
+      </figure>
+      <div class="content__rank--title">
+        <div class="content__rank--title-text">
+        ${data.title}
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
 }
 
-main();
+const animeHTMLEl1 = document.querySelector(".contents__first-4")
+const animeHTMLEl2 = document.querySelector(".content-remains")
+
+async function animeMain() {
+  const anime = await fetch(`https://api.jikan.moe/v4/anime`);
+  const animePortal = await anime.json();
+  const animeData1 = animePortal.data.slice(0, 4);
+  const animeData2 = animePortal.data.slice(5, 25)
+  console.log(animeData1);
+  console.log(animeData2);
+  animeHTMLEl1.innerHTML = animeData1.map((data) => animeHTML1(data)).join("");
+  animeHTMLEl2.innerHTML = animeData2.map((data) => animeHTML2(data)).join("");
+  // console.log(animeHTMLEl1)
+}
+animeMain();
+
+function animeHTML1(data) {
+  return `
+            <div class="contents__first-4">
+              <div class="contents">
+                <div class="content">
+                  <div class="content__wrapper">
+                    <figure class="content__img--wrapper">
+                      <img
+                        class="content__img"
+                        src= ${data.images.webp.image_url}
+                        alt=""
+                      />
+                    </figure>
+                    <div class="content__wrapper--bg"></div>
+                    <p class="content__description">
+                      ${data.synopsis}
+                    </p>
+                  </div>
+                  <div class="content__title">
+                    <div class="content__title-text">
+                      ${data.title}
+                    </div>
+                  </div>
+                </div>
+              </div>              
+            </div>              
+  `
+}
+
+// console.log(animeHTML1)
+
+function animeHTML2(data) {
+  return `
+              <div class="content2">
+                <div class="content__wrapper">
+                  <figure class="content__img--wrapper">
+                    <img
+                      class="content__img"
+                      src= ${data.images.webp.image_url}
+                      alt=""
+                    />
+                  </figure>
+                  <div class="content__wrapper--bg"></div>
+                  <p class="content__description">
+                    ${data.synopsis}
+                  </p>
+                </div>
+                <div class="content__title">
+                  <div class="content__title-text">
+                    ${data.title}
+                  </div>
+                </div>
+              </div>
+              
+  `
+}
+
+
+
+
+
 
 function showAnimeContent(id) {
   localStorage.setItem("id", id);
   window.location.href = `${window.location.origin}/anime-list.html`;
 }
-
-function animeHTML(data) {
-  return `<div class="content__rank--wrapper" (${data.id})>
-            <figure class="content__rank--img--wrapper">
-              <img
-                src="${data.images.webp.image_url}"
-                class="content__rank--img"
-              />
-              <div class="content__rank">1</div>
-            </figure>
-            <div class="content__rank--title">
-              <div class="content__rank--title-text">
-                The Aristocrat's Otherworldly Adventure
-              </div>
-            </div>
-          </div>
-  `;
-}
-
-console.log(animeHTML)
-
 window.addEventListener("scroll", () => {
   const currentScrollY = window.scrollY;
   const navbar = document.querySelector(".nav2");
